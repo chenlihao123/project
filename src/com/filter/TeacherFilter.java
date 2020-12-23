4771c3e3bab55d17f2aaf83aca368df498de9ae8
chenlihao123 <821865057@qq.com>
2020-12-23 23:38:20 +0800

    1223 2338

---
+++ src/com/filter/TeacherFilter.java
@@ -0,0 +1,39 @@
+package com.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author chenlihao
+ * @create 2020-12-23 23:18
+ */
+@WebFilter(urlPatterns ={"/home/*","/teacher.html"})
+public class TeacherFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+        HttpServletResponse httpServletResponse=(HttpServletResponse)resp;
+        HttpSession session = httpServletRequest.getSession();
+        Object admin = session.getAttribute("admin");
+        Object teacher = session.getAttribute("teacher");
+        // 如果等于 null，说明还没有登录
+        if (admin == null&&teacher==null) {
+            httpServletResponse.sendRedirect("http://localhost:8080/onlinelearning/teacherLogin.html");
+            return;
+        } else {
+// 让程序继续往下访问用户的目标资源
+            chain.doFilter(req, resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
